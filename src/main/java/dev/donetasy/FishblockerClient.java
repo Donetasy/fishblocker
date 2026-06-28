454f53c547f7215cf4ae39583623d578a7262024
Donetasy <daniel2021@euteneuer.com>
2026-06-28 14:49:28 +0200

    no changes

--- src/main/java/dev/donetasy/FishblockerClient.java
+++ src/main/java/dev/donetasy/FishblockerClient.java
@@ -6,6 +6,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class FishblockerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // Hier kommt client-spezifischer Code rein
+        
     }
 }
